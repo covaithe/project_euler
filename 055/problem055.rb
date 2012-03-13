5957bee95de1eaefdc1caba39b061a2c364a0307
Greg Fleming <covaithe@gmail.com>
2012-03-13 08:52:30 -0400

    much faster using string conversions rather than digit parsing

--- 055/problem055.rb
+++ 055/problem055.rb
@@ -1,29 +1,5 @@
 #!/usr/bin/env ruby
 
-def digits_of(n, base=10)
-  digits = []
-  while n > 0
-    digits.push(n%base)
-    n /= base
-  end
-  return digits.reverse
-end
-
-def assemble(digits)
-  digits.inject(0) {|answer,i| answer = answer*10 + i}
-end
-
-def is_palindrome?(n, base=10)
-  digits = digits_of(n, base)
-  backwards = digits.reverse
-  until digits.length == 0
-    a = digits.pop()
-    b = backwards.pop()
-    return false if a != b
-  end
-  return true
-end
-
 class Integer
   def is_lychrel?
     iteration = 1
@@ -31,7 +7,7 @@ class Integer
     while iteration <= 50 do
       reversed = r.reverse
       sum = r + reversed
-      if is_palindrome? sum
+      if sum.is_palindrome?
         return false 
       end
 
@@ -42,7 +18,12 @@ class Integer
   end
 
   def reverse
-    assemble(digits_of(self).reverse)
+    to_s.reverse.to_i
+  end
+
+  def is_palindrome?
+    s = to_s
+    s == s.reverse
   end
 
 end
